50d46cb1e8d4685d1c87f7d5050e1c83011dd13a
a3668 <yahoo52042@gmail.com>
2024-12-28 23:05:58 +0800

    add README.md

--- hw1/oakMe.js
+++ hw1/oakMe.js
@@ -1,12 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
+//建立一個物件叫app
 
+//ctx 是 Oak 的上下文物件 (context)，包含了請求 (ctx.request) 和回應 (ctx.response) 的相關資料。
 app.use((ctx) => {
   // ctx.response.status = 404
   console.log('url=', ctx.request.url)
   let pathname = ctx.request.url.pathname
-  if (pathname == '/') {
+  if (pathname == '/') {//
     ctx.response.body = `<html>
 <body>
 <h1>我的自我介紹</h1>
